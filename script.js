1531b8d2acc3626833b0ecdf29d89b8bcfdb78fc
amanicps <80164449+AmaniMango@users.noreply.github.com>
2024-03-11 14:35:20 +0000

    done with if

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
-let q1 = (".Q1");
-let q2 = (".Q2");
-
+let input1 = document.querySelector(".q1");
+let input2 = document.querySelector(".q2");
+let button = document.querySelector("button");
+let message = document.querySelector(".message");
 console.log("hi");
 
 button.addEventListener("click", function(){
-    if(!Q1 || !Q2){
-        message.innerHTML = "You need to enter SOMETHING. ";
-        }
-        else if (q1 === "yes" && q2 ==="sweet"){
-        console.log("success");
-            message.innerHTML ="Success!";
-        }
+    let answer1 = input1.value;
+    let answer2 = input2.value;
+if (!answer1 || !answer2){
+    message.innerHTML = "Please Input something";
+
+}
+//Tikki
+else if (answer1 === "yes" && answer2 === "sweet"){
+    message.innerHTML = "Tikki";
+}
+//Nooroo
+else if (answer1 === "yes" && answer2 === "savory"){
+    message.innerHTML = "Nooroo";
+}
+//Plagg
+else if (answer1 === "no" && answer2 === "savory"){
+    message.innerHTML = "Plagg";
+}
+//Duusuu
+else if (answer1 === "no" && answer2 === "sweet"){
+    message.innerHTML = "Duusuu";
+}
+//Make it so when the results come in, the image of the character they got reveals.
+
+    // if(!q1){
+        // document.querySelector("message").innerHTML = "enter something";
+
+    // }
+// if (q1 === "yes"){
+    // 
+// }
+
+    // if(!q1 || !q2){
+    //     message.innerHTML = "You need to enter SOMETHING. ";
+    //     }
+    //     else if (q1 === "yes" && q2 ==="sweet"){
+    //     console.log("success");
+    //         message.innerHTML ="Success!";
+    //     }
 });
