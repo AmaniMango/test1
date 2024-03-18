ff647288fb57e5285bbe3e429ce425065ab19990
amanicps <80164449+AmaniMango@users.noreply.github.com>
2024-03-18 13:22:24 +0000

    image displays attempt

--- script.js
+++ script.js
@@ -4,28 +4,61 @@ let button = document.querySelector("button");
 let message = document.querySelector(".message");
 console.log("hi");
 
+let plagg = document.querySelector(".plagg");
+let tikki = document.querySelector(".tikki");
+let duusuu = document.querySelector(".duusuu");
+let nooroo = document.querySelector(".nooroo");
+
+
+
+plagg.style.display = "none";
+tikki.style.display = "none";
+nooroo.style.display = "none";
+duusuu.style.display = "none";
+
 button.addEventListener("click", function(){
     let answer1 = input1.value;
     let answer2 = input2.value;
+
 if (!answer1 || !answer2){
     message.innerHTML = "Please Input something";
-
+    plagg.style.display = "none";
+    tikki.style.display = "none";
+    nooroo.style.display = "none";
+    duusuu.style.display = "none";
 }
+
 //Tikki
 else if (answer1 === "yes" && answer2 === "sweet"){
     message.innerHTML = "Tikki";
+    plagg.style.display = "none";
+    tikki.style.display = "block";
+    nooroo.style.display = "none";
+    duusuu.style.display = "none";
 }
 //Nooroo
 else if (answer1 === "yes" && answer2 === "savory"){
     message.innerHTML = "Nooroo";
+    plagg.style.display = "none";
+    tikki.style.display = "none";
+    nooroo.style.display = "block";
+    duusuu.style.display = "none";
 }
 //Plagg
 else if (answer1 === "no" && answer2 === "savory"){
     message.innerHTML = "Plagg";
+    plagg.style.display = "block";
+    tikki.style.display = "none";
+    nooroo.style.display = "none";
+    duusuu.style.display = "none";
 }
 //Duusuu
 else if (answer1 === "no" && answer2 === "sweet"){
     message.innerHTML = "Duusuu";
+    plagg.style.display = "none";
+    tikki.style.display = "none";
+    nooroo.style.display = "none";
+    duusuu.style.display = "block";
 }
 //Make it so when the results come in, the image of the character they got reveals.
 
@@ -44,4 +77,5 @@ else if (answer1 === "no" && answer2 === "sweet"){
     //     console.log("success");
     //         message.innerHTML ="Success!";
     //     }
+    
 });
